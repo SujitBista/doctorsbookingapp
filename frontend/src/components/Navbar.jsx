9e99aa15698bc6245511c85d35b0bfc34ad0454c
Sujit Bista <sujitbistaprogrammer@gmail.com>
2025-06-19 13:34:46 +0545

    setup svgr in vite config file for importing svg image inorder to create angle dropdown UI of dropdown menu and created dropdown menus like  profile picture, my appointments and logout"

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, {useState} from 'react'
+import { NavLink, useNavigate } from 'react-router-dom'
 import logo from '../assets/logo.png'
+import  DropdownIcon from '../assets/angle-dropdown.svg?react'
 
 const Navbar = () => {
+  const navigateToRoute = useNavigate();
+  const [showMenu, setShowMenu] = useState(false);
+  const [token, setToken] = useState(true);
+
   const navLink = [
     {label: 'HOME', to: '/'},
     {label: 'ABOUT', to: '/about'},
@@ -31,8 +36,22 @@ const Navbar = () => {
           { nav }
       </ul>
 
-      <div>
-        <button className="bg-blue-400 rounded-full p-3 text-white font-bold cursor-pointer">Create Account</button>
+      <div className="flex items-center">
+        {
+          token 
+          ? <div className="flex items-center gap-1 cursor-pointer group relative">
+              <img className="w-8 rounded-full" src={`https://placehold.co/40x40`} alt="" />
+              <DropdownIcon className="w-6 h-5"/>
+              <div className="absolute top-0 right-0 pt-14 text-base z-20 text-gray-600 font-medium hidden group-hover:block">
+                <div className="mt-5 min-w-48 rounded bg-stone-100 p-2 flex flex-col gap-4">
+                    <p onClick= {() => navigateToRoute('my-profile')} className="hover:text-black cursor-pointer">My Profile</p>
+                    <p onClick= {() => navigateToRoute('my-appointments')} className="hover:text-black cursor-pointer">My Appointments</p>
+                    <p onClick = {()=> {setToken(false)}} className="hover:text-black cursor-pointer" >Logout</p>
+                </div>
+              </div>
+          </div>
+          : <button onClick={() => {navigateToRoute('/login'); setToken(true)}} className="bg-blue-400 rounded-full p-3 text-white font-bold cursor-pointer md:block">Create Account</button>
+        }
       </div>
     </div>
   )
