41eeef9eb8903362f0604a55df9ef062e1e3b49f
Sujit Bista <sujitbistaprogrammer@gmail.com>
2025-06-21 20:03:14 +0545

    few changes made to header section

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        {/*------ Left Side ----- */}
+        <div>
+            <p>Book Appointment <br/> With Trusted Doctors</p>
+            <img src="" alt="group images" />
+            <p>Simply browse through our extensive list of trusted doctors, <br/> schedule your appointment hassle-free.</p>
+            <a href="">Book appointment </a>
+        </div>
+        {/*------ Right Side ----- */}
+        <div>
+            <img src="src/assets/banner_doctors.png" />
+        </div>
+    </div>
+  )
+}
+
+export default Header
