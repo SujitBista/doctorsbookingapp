6bd4678f12a7f8f6f3376f0d86c553c4d373e914
Sujit Bista <sujitbistaprogrammer@gmail.com>
2025-06-29 13:26:08 +0545

    added speciality section and exported images from assets.js file'

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,45 @@
+import banner_doctors from './banner_doctors.png'
+import group_profile from './group_profile.png'
+import logo from './logo.png'
+import rightArrow from './right-arrow.svg'
+import General_Physician from './General_Physician.png'
+import Gynecologist from './Gynecologist.png'
+import Dermatologist from './Dermatologist.png'
+import Pediatricians from './Pediatricians.png'
+import Neurologist from './Neurologist.png'
+import Gastroenterologist from './Gastroenterologist.png'
+
+export const assets = {
+    banner_doctors,
+    group_profile,
+    logo,
+    rightArrow
+}
+
+export const specialityData = [
+    {
+        speciality: 'General Physician',
+        image: General_Physician
+    },
+    {
+        speciality: 'Gynecologist',
+        image: Gynecologist
+    },
+    {
+        speciality: 'Dermatologist',
+        image: Dermatologist
+    },
+    {
+        speciality: 'Pediatricians',
+        image: Pediatricians
+    },
+    {
+        speciality: 'Neurologist',
+        image: Neurologist
+    },
+    {
+        speciality: 'Gastroenterologist',
+        image: Gastroenterologist
+    },
+]
+
---
+++ frontend/src/components/SpecialityMenu.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {specialityData} from '../assets/assets'
+import { Link } from 'react-router-dom'
+
+const SpecialityMenu = () => {
+  return (
+    <div className="py-16" id="speciality">
+        <h1 className="text-4xl">Find by Speciality</h1>
+        <p className="text-bold">Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free</p>
+        <div className="flex gap-4 pt-5 overflow-scroll w-full">
+            {specialityData.map((item, index) => (
+                <div key={item}>
+                    <div>
+                        <Link onClick={()=> {onscroll(0,0)}} className="hover:-translate-y-3 transition-all duration-500 inline-block" key={index} to={`/doctors/${item.speciality}`}>
+                            <img className="w-20 h-20 sm:w-24 sm:h-24 md:w-60 md:h-60 rounded-full" src={item.image}/>
+                            <div className="ml-12 font-bold mb-2">{item.speciality}</div>
+                        </Link>
+                    </div>
+                </div>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default SpecialityMenu
